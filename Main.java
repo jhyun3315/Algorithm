3f9835723461a88b84d04031b8bda6f679d40f7d
JHYUN <37072549+jhyun3315@users.noreply.github.com>
2021-10-26 00:05:45 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int test = Integer.parseInt(br.readLine());
+
+        for(int i=0;i<test;i++){
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str," ");
+
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int n =Integer.parseInt(st.nextToken());
+
+        }
+
+    }
+}
