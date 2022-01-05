d35c22c547c636837bfd057f73b51ee11a0c79f8
JHYUN <37072549+jhyun3315@users.noreply.github.com>
2022-01-06 01:21:07 +0900

    Create Backjoon1436.java

---
+++ Backjoon1436.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int num = 666;
+        int count = 1;
+
+        while (count != N) {
+            num++;
+
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
